69fc2607839be9d780a3bd08b44ff414d4dd4a07
CarlTobias <carlamirtobias@gmail.com>
2023-04-16 14:48:20 +0400

    chapter 5

--- SetExercises-main/Chapter 5/Alan Partridge Soundboard/soundboard.js
+++ SetExercises-main/Chapter 5/Alan Partridge Soundboard/soundboard.js
@@ -1,4 +1,44 @@
 function ahha(){
-    var snd = new Audio("Ah-Ha.mp3")
+    var snd = new Audio("Ah-Ha.mp3");
+    snd.play()
+}
+
+function dan(){
+    var snd = new Audio("Dan.mp3");
+    snd.play()
+}
+
+function backofthenet(){
+    var snd = new Audio("Back of the Net.mp3");
+    snd.play()
+}
+
+function bangoutoforder(){
+    var snd = new Audio("Bang Out Of Order.mp3");
+    snd.play()
+}
+
+function jurassicpark(){
+    var snd = new Audio("Jurassic Park.mp3");
+    snd.play()
+}
+
+function smellmycheese(){
+    var snd = new Audio("Smell My Cheese.mp3");
+    snd.play()
+}
+
+function goal(){
+    var snd = new Audio("Goal.mp3");
+    snd.play()
+}
+
+function kissmyface(){
+    var snd = new Audio("Kiss my Face.mp3");
+    snd.play()
+}
+
+function hellopartridge(){
+    var snd = new Audio("Hello Partridge.mp3");
     snd.play()
 }
